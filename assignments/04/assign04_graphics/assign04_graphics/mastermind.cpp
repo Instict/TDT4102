9aff936bfc23c938597c0a2526943a2dffdb40ac
Marius <marius.blom@knett.no>
2019-02-07 19:16:43 +0100

    uploaded wrong project, graphics done

--- assignments/04/assign04_graphics/assign04_graphics/mastermind.cpp
+++ assignments/04/assign04_graphics/assign04_graphics/mastermind.cpp
@@ -6,29 +6,42 @@
 #include "masterVisual.h"
 
 void playMastermind() {
-	int win_w = 400;
-	int win_h = 600;
-	MastermindWindow mwin{ Point{900,20}, win_w, win_h, "Mastermind" };
+
+	constexpr int win_w = 400;
+	constexpr int win_h = 600;
+
 	constexpr int size = 4;
 	constexpr int letters = 6;
+	//	dummy variables for troubleshooting
+	int correctPosition;
+	int correctCharacter;
+
 	string code;
 	string guess;
-	int tries = 2;
-	
-	code = randomizeString(size, 'A' + (letters - 1), 'A');
-	//code = "DDBA";
+	int tries = 10;
+	int round = 0;
 
-	
+	code = randomizeString(size, 'A' + (letters - 1), 'A');
+	//code = "BEFE";
+
 	cout << endl;
 	cout << "code: ";
 	for (int i = 0; i < code.size(); i++) {
 		cout << code[i];
 	}
 
+
+	MastermindWindow mwin{ Point{900,20}, win_w, win_h, "Mastermind" };
+	//	add the solution for troubleshooting
+	addGuess(mwin, code, size, 'A', round);
+	hideCode(mwin, size);
 	do{
+		mwin.wait_for_button();
+		round++;
 		cout << endl;
 		cout << "You have " << tries << " tries left!" << endl;
 		guess = readInputToString(size);
+		//guess = "ABCD";	//	####	BUILD ONLY		####
 		cout << "guess: ";
 		for (int i = 0; i < guess.size(); i++) {
 			cout << guess[i];
@@ -37,11 +50,21 @@ void playMastermind() {
 		cout << "correct spot: " << checkCharacters(code, guess) << endl;
 		cout << "correct color: " << checkCharactersAndPosition(code, guess) << endl;
 		tries--;
+		addGuess(mwin, guess, size, 'A', round);
+		correctPosition = checkCharacters(code, guess);
+		correctCharacter = checkCharactersAndPosition(code, guess);
+		addFeedback(mwin, correctPosition, correctCharacter, size, round);
+
+		//mwin.wait_for_button();
 	} while (checkCharacters(code, guess) < code.size() && tries > 0);
 	if (checkCharacters(code, guess) == code.size()) {
+		addGuess(mwin, code, size, 'A', 0);
 		cout << "You won!" << endl;
 	}
 	else {
+		addGuess(mwin, code, size, 'A', 0);
 		cout << "You lost!" << endl;
 	}
+
+	mwin.wait_for_button();
 }
